ff2aa9e3b730913c3a036ca4b8ed128d70254537
Vien V.Nguyen <vien.nguyen@sutrixsolutions.com>
2016-06-22 10:26:15 +0700

    Init project

---
+++ App.day/App.day/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App.day
+//
+//  Created by iOS Dev on 6/22/16.
+//  Copyright © 2016 iOS Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
